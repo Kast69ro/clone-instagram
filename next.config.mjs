6ed7842b1a931d17f39e41b3e4f84d14f00ba4d9
Kastro <boboevdjasur@gmail.com>
2025-08-02 09:25:00 +0600

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 const nextConfig = {
   output: "export", // 👈 добавь эту строку
-  i18n: {
-    locales: ["en", "tj"],
-    defaultLocale: "en",
-    localeDetection: false,
-  },
+  // i18n: {
+  //   locales: ["en", "tj"],
+  //   defaultLocale: "en",
+  //   localeDetection: false,
+  // },
   images: {
     domains: ["37.27.29.18", "instagram-api.softclub.tj"],
   },
